7f8acbc144f22ecb8ae170e74200aaed10afa3aa
Romulo Esquivel <52230585+romuloesquivel1@users.noreply.github.com>
2023-11-28 20:21:52 +0700

    update env

--- src/lib/supabaseClient.js
+++ src/lib/supabaseClient.js
@@ -1,5 +1,4 @@
 import { createClient } from '@supabase/supabase-js';
-const supabaseUrl = 'https://yshoijtkskphgmgszidp.supabase.co';
-const supabaseToken = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InlzaG9panRrc2twaGdtZ3N6aWRwIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTk5MzY1OTksImV4cCI6MjAxNTUxMjU5OX0.YU2yJaJtJ8KPitry1F1NOPDwuGy9Z_MVArA1NQz46ik';
 
-export const supabase = createClient(supabaseUrl, supabaseToken);
+console.log('import.meta.env', import.meta.env)
+export const supabase = createClient(import.meta.env.VITE_SUPABASE_URL, import.meta.env.VITE_SUPABASE_TOKEN);
